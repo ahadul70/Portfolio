639c59d3498ed23ce6796394c544c270d7416d6a
Ahadul70 <ahadul70@gmail.com>
2025-05-01 16:56:58 +0600

    finished making the portfolio

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I am currently a competitive programer, I worked on projects you can see 
+    here <a href="https://github.com/ahadul70" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. i finished my gradation form North South University`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="../Resume-Ahadul-Islam-Software Developer.pdf" target="_blank">Check it out?</a>
+  Contact me at <a href="https://mail.google.com/mail/u/0/?tab=wm#inbox?compose=GTvVlcSKjRVjJZxKHXHbQgwTVKMlMrGZpRrMCwVgbHdnlqwVvSDFKwnBZZkSPGwLZLkLMSQrdFVKh" target="_blank" >ahadul70@gmail.com</a> if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves.
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Ahadul's , just close the tab.`,
+};
